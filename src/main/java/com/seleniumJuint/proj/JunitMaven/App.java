9addf440141c0f671e37b98dc33c71b98f799f83
unknown <sonamr@sonamr.cybage.com>
2017-06-06 12:08:48 +0530

    random commit

---
+++ src/main/java/com/seleniumJuint/proj/JunitMaven/App.java
@@ -0,0 +1,13 @@
+package com.seleniumJuint.proj.JunitMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
